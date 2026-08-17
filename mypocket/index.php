ad9e91158efe3aaa43d1fc9c3371014d686e1d1e
Larissa Peters <peters2009.364@gmail.com>
2026-08-17 07:48:35 -0300

    Atualizar o index.php

--- mypocket/index.php
+++ mypocket/index.php
@@ -21,7 +21,6 @@ $carteira = new Carteira($pdo);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body class="bg-light">
-//coiso do erro
 <div class="container py-5">
 <?php if (isset($_SESSION['erro'])): ?>
 
