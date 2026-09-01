9c1ef0a254a87a24ae87f06e7d43e789a28efd98
Larissa Peters <peters2009.364@gmail.com>
2026-09-01 10:23:30 -0300

    Add login.php for user authentication

---
+++ MyPocket atualizado sem virus 2026 real/login.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyPocket</title>
+
+</head>
+
+<body>
+    <div>
+        <form>
+            <label for="email">Cadastrara</label>
+            <div>
+                <label for="email">Email Address</label>
+                <input type="email" id="email" name="email" placeholder="name@example.com" required>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <button type="submit">Ciar conta</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
